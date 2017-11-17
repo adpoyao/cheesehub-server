ed0251b9cf6d4811decc64464a78f4f5a4b13c34
Andy Gaines <andyagaines@gmail.com>
2017-11-17 14:43:28 -0600

    deployed

--- index.js
+++ index.js
@@ -3,63 +3,67 @@
 const express = require('express');
 const cors = require('cors');
 const morgan = require('morgan');
-
+const bodyParser = require('body-parser');
 const {PORT, CLIENT_ORIGIN} = require('./config');
 const {dbConnect} = require('./db-mongoose');
 // const {dbConnect} = require('./db-knex');
-
+const jsonParser = bodyParser.json();
 const app = express();
 
 const cheeses = [
-  'Bath Blue',
-  'Barkham Blue',
-  'Buxton Blue',
-  'Cheshire Blue',
-  'Devon Blue',
-  'Dorset Blue Vinney',
-  'Dovedale',
-  'Exmoor Blue',
-  'Harbourne Blue',
-  'Lanark Blue',
-  'Lymeswold',
-  'Oxford Blue',
-  'Shropshire Blue',
-  'Stichelton',
-  'Stilton',
-  'Blue Wensleydale',
-  'Yorkshire Blue'
+	'Bath Blue',
+	'Barkham Blue',
+	'Buxton Blue',
+	'Cheshire Blue',
+	'Devon Blue',
+	'Dorset Blue Vinney',
+	'Dovedale',
+	'Exmoor Blue',
+	'Harbourne Blue',
+	'Lanark Blue',
+	'Lymeswold',
+	'Oxford Blue',
+	'Shropshire Blue',
+	'Stichelton',
+	'Stilton',
+	'Blue Wensleydale',
+	'Yorkshire Blue'
 ];
 
 app.use(
-  morgan(process.env.NODE_ENV === 'production' ? 'common' : 'dev', {
-    skip: (req, res) => process.env.NODE_ENV === 'test'
-  })
+	morgan(process.env.NODE_ENV === 'production' ? 'common' : 'dev', {
+		skip: (req, res) => process.env.NODE_ENV === 'test'
+	})
 );
 
 app.use(
-  cors({
-    origin: CLIENT_ORIGIN
-  })
+	cors({
+		origin: CLIENT_ORIGIN
+	})
 );
 
 app.get('/api/cheeses', (req, res) => {
-  res.send(cheeses);}
+	res.send(cheeses);}
 );
 
+app.post('/api/cheeses', jsonParser, (req, res) => {
+	res.send('Posted.');
+});
+
 function runServer(port = PORT) {
-  const server = app
-    .listen(port, () => {
-      console.info(`App listening on port ${server.address().port}`);
-    })
-    .on('error', err => {
-      console.error('Express failed to start');
-      console.error(err);
-    });
+	const server = app
+		.listen(port, () => {
+			console.info(`App listening on port ${server.address().port}`);
+		})
+		.on('error', err => {
+			console.error('Express failed to start');
+			console.error(err);
+		});
 }
 
 if (require.main === module) {
-  dbConnect();
-  runServer();
+	dbConnect();
+	runServer();
 }
 
 module.exports = {app};
